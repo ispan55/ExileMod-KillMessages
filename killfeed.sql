2e4bd340da973431c2779b09f2623c777f507659
Gr8 <gr8_boi52@yahoo.com>
2015-11-10 19:30:57 -0800

    typo

--- killfeed.sql
+++ killfeed.sql
@@ -6,7 +6,7 @@ CREATE TABLE `kills` (
 	`victim_uid` VARCHAR(32) NULL DEFAULT NULL COMMENT 'UID of the Victim',
 	`weapon` VARCHAR(50) NOT NULL DEFAULT 'Unkown' COMMENT 'Name of the weapon/vehicle or item used to kill',
 	`distance` VARCHAR(10) NOT NULL DEFAULT '0' COMMENT 'Number of distance in meters between the killer and the victim',
-	`repect` VARCHAR(10) NULL DEFAULT '0' COMMENT 'Number of respect the killer earned',
+	`respect` VARCHAR(10) NULL DEFAULT '0' COMMENT 'Number of respect the killer earned',
 	`died_at` DATETIME NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Date and time the victim was killed',
 	PRIMARY KEY (`id`)
 )
